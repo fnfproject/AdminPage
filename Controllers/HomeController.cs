03128bac3b66b7affd3b12313181115f7dde63c5
fnfproject <fnfproject2024@gmail.com>
2024-08-24 14:42:21 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult TrainerLogin()
+        {
+            return View();
+        }
+    }
+}
